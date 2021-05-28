b829e6c36c5c776d3cb56757500597b6ad63b72a
Alex Novak <alexleonnovak@gmail.com>
2021-05-28 16:07:53 +0300

    add TransactionHistory component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import 'modern-normalize/modern-normalize.css';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 import userProfileData from './assets/json/user.json';
 import statisticalData from './assets/json/statistical-data.json';
 import friendsData from './assets/json/friends.json';
+import transactionsData from './assets/json/transactions.json';
 
 import './App.css';
 
@@ -23,6 +25,7 @@ function App() {
 			<Statistics title="Upload stats" stats={statisticalData} />
 			<Statistics stats={statisticalData} />
 			<FriendList friends={friendsData} />
+			<TransactionHistory transactions={transactionsData} />
 		</div>
 	);
 }
